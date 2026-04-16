2dd638164b83c27b263db77ec07990f44b715a53
Stepan <afanasevstepan67@gmail.com>
2026-04-16 12:26:06 +0400

    Initialize UI projecеt

---
+++ src/test/java/com/sdet/sdet_practice/SdetPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdet.sdet_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
